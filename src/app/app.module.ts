8d1a8f4435bf79d8c0a35c557605ad359ffab636
Diego <disorder1977@gmail.com>
2023-04-07 19:29:39 -0300

    Se crean componentes del header y el nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
+import { MainHomeComponent } from './components/main-home/main-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderHomeComponent,
+    MainHomeComponent
   ],
   imports: [
     BrowserModule,
